c72bb374a8a77c1a489aac330394684cd3b8ad94
eggtoasts <129806552+eggtoasts@users.noreply.github.com>
2025-09-30 18:19:32 -0400

    Added task & project classes, dialog UI, event handling, and styling updates - Added priority-based color indicators for tasks - Added dialog UI for creating new tasks with inputs (title, date, etc.) - Handled button events to trigger dialog visibility - Refined styling for the main project page and sidebar

--- src/index.js
+++ src/index.js
@@ -11,14 +11,23 @@ function Projects() {
     projectsArray.push(project_obj);
   };
 
-  return { getProjectsArray };
+  return { getProjectsArray, addProject };
 }
 
 class Project {
   //todo array is an array of todo item objects
-  constructor(name, color, todosArray, id) {}
+  constructor(name, color, id) {
+    this.name = name;
+    this.color = color;
+    this.todosArray = [];
+    this.id = id;
+  }
 
   get getTodosSize() {}
+
+  addTodo(todo) {
+    this.todosArray.push(todo);
+  }
 }
 
 class Todos {
@@ -63,6 +72,67 @@ function deleteTodo(todosArray) {
 
 //DOM stuff
 
+const Proj = Projects("Project 1", "Red", 1);
+const task = new Todos("w", "w", "11-11-11", "None", 0);
+const randomProject = new Project("Random_Task", "Blue", [], 1);
+Proj.addProject(randomProject);
+randomProject.addTodo(task);
+
+console.log(randomProject.todosArray);
+
+function clearProjectUI(projectContainer) {
+  let projectChild = projectContainer.lastElementChild;
+
+  while (projectChild != null) {
+    projectContainer.removeChild(projectChild);
+    projectChild = projectContainer.lastElementChild;
+  }
+}
+
+function clearTaskUI(taskContainer) {
+  let taskChild = taskContainer.lastElementChild;
+
+  while (taskChild != null) {
+    taskContainer.removeChild(taskChild);
+    taskChild = taskContainer.lastElementChild;
+  }
+}
+
+function updateProjectsUI(projectContainer, projectsArrau) {
+  clearProjectUI(projectContainer);
+}
+
+function updateTasksUI(taskContainer, todosArray) {
+  clearTaskUI(taskContainer);
+  todosArray.forEach((e) => {
+    //make a div object
+    //then add it into the task container
+    //createProjectItem();
+  });
+}
+
+function createProjectItem() {}
+
 const UserInterface = function () {
+  //Selecting project container
+  const projectContainer = document.querySelector(".projects-container");
+  const taskContainer = document.querySelector(".task-container");
+  console.log(taskContainer);
+
+  //button
+  const addTaskButton = document.querySelector(".add-task");
+  const addTaskDialog = document.querySelector("dialog");
+
+  addTaskButton.addEventListener("click", (e) => {
+    console.log("Bruh");
+    addTaskDialog.showModal();
+  });
+
+  //Updates UI of Project & Tasks (commented out for now)
+  // updateProjectsUI(projectContainer);
+  // updateTasksUI(taskContainer);
+
   return {};
 };
+
+UserInterface();
